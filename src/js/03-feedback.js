29bf15643f3e25d61b55dc8a3d98db68d653c9d2
Oleg Kirpa <profix.ok@gmail.com>
2023-02-19 19:21:16 +0200

    task3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,33 @@
+import throttle from 'lodash.throttle';
 
+const formEl = document.querySelector('.feedback-form');
+const formData = {};
+const LOCALSTORAGE_KEY = 'feedback-form-state';
+
+initForm();
+
+formEl.addEventListener('submit', onFormSubmit);
+formEl.addEventListener('input', throttle(onFormInput, 500));
+
+function onFormInput(event) {
+  formData[event.target.name] = event.target.value;
+  console.log('formData:', formData);
+  return localStorage.setItem(LOCALSTORAGE_KEY, JSON.stringify(formData));
+}
+
+function onFormSubmit(evt) {
+  evt.preventDefault();
+  evt.currentTarget.reset();
+  localStorage.removeItem(LOCALSTORAGE_KEY);
+}
+
+function initForm() {
+  const saveText = JSON.parse(
+    localStorage.getItem(LOCALSTORAGE_KEY, JSON.stringify(formData))
+  );
+
+  if (saveText) {
+    formEl.email.value = saveText.email;
+    formEl.message.value = saveText.message;
+  }
+}
